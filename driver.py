75a4a7d57f711476a511ed0ca6e8ef281d028d82
Shreyas Nidugala <shreyasnidugala@gmail.com>
2022-10-30 16:05:54 +0530

    driver main added parsing phase

--- driver.py
+++ driver.py
@@ -1,7 +1,8 @@
 from buffer import load_buffer
 from lex import tokenize
-
+from parser import *
 if __name__ == "__main__":
+    # LEXICAL ANALYSIS PHASE
     token = []
     lexeme = []
 
@@ -13,3 +14,11 @@ if __name__ == "__main__":
 
     res = ", ".join(set(token))
     print("\n\n Unique tokens: \n", res)
+
+    # PARSING PHASE
+    parse_grammar()
+    items()
+    global parse_table
+    parse_table = [[""] for c in range(len(terminals) + len(nonterminals) + 1)for r in range(len(C))]
+    print_info()
+    process_info()
