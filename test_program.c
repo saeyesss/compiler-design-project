9a39256ea7caf273f93231acb2c098e8d38699ec
Shreyas Nidugala <shreyasnidugala@gmail.com>
2022-12-07 22:28:23 +0530

    custom grammar finalized. Edited source C program

--- test_program.c
+++ test_program.c
@@ -1,19 +1,22 @@
 //Q2: Find the largest number using if statement
-int main(){
-    int n1 , n2 , n3;
-    if(n1 >= n2){
-        if(n1 >= n3){
-            printf("%d", n1);
+int main () {
+    int n1 , n2 , n3 ;
+    if ( n1 >= n2 ) {
+        if ( n1 >= n3 ) {
+            printf ( "%d" , n1 ) ;
         }
-        else{
-            printf("%d", n3);
+        else {
+            printf ( "%d" , n3 ) ;
         }
-    } else{
-        if(n2 >= n3){
-            printf("%d", n2);
+    }
+    else {
+        if ( n2 >= n3 ) {
+            printf ( "%d" , n2 ) ;
         }
-        else{
-            printf("%d", n3);
+        else {
+            printf ( "%d" , n3 ) ;
         }
     }
 }
+
+int main () { int n1 , n2 , n3 ; if ( n1 >= n2 ) { if ( n1 >= n3 ) { printf ( "%d" , n1 ) ; } else { printf ( "%d" , n3 ) ; } } else { if ( n2 >= n3 ) { printf ( "%d" , n2 ) ; } else { printf ( "%d" , n3 ) ; } } }
