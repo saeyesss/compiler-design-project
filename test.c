033b2c4d3d0d703a3a197d67553787e534a3bf0d
Shreyas Nidugala <shreyasnidugala@gmail.com>
2022-12-09 09:40:28 +0530

    added test.c

---
+++ test.c
@@ -0,0 +1,1 @@
+int main () { int n1 , n2 , n3 ; if ( n1 >= n2 ) { if ( n1 >= n3 ) { printf ( "%d" , n1 ) ; } else { printf ( "%d" , n3 ) ; } } else { if ( n2 >= n3 ) { printf ( "%d" , n2 ) ; } else { printf ( "%d" , n3 ) ; } } }
--- test_program.c
+++ test_program.c
@@ -18,5 +18,3 @@ int main () {
         }
     }
 }
-
-int main () { int n1 , n2 , n3 ; if ( n1 >= n2 ) { if ( n1 >= n3 ) { printf ( "%d" , n1 ) ; } else { printf ( "%d" , n3 ) ; } } else { if ( n2 >= n3 ) { printf ( "%d" , n2 ) ; } else { printf ( "%d" , n3 ) ; } } }
